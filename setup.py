17c86ec24ad5ee365a8311d4b70343e941a326dc
Suchita Meshram <suchita.meshram@fractal.ai>
2023-03-02 22:57:08 +0530

    GLOBALMART_FUNCTION_BDIST

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='globalmart_api',
+version='0.1',
+description='Testing installation of Package',
+author='Suchita Meshram',
+author_email='suchitameshram28@gmail.com',
+license='MIT',
+install_requires=['pandas'],
+packages=['globalmart_api'],
+)
